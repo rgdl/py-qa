126a5b4eada2ba3e3f44f547c1883c252ffc3d4e
rgdl <robert_delisle44@hotmail.com>
2022-06-19 21:10:54 +1000

    more configuration

--- main.sh
+++ main.sh
@@ -4,21 +4,21 @@ COLOUR="\033[0;35m"
 NC="\033[0m"
 
 echo "${COLOUR}"
-echo "LINTING"
+echo "LINTING (flake8)"
 echo $NC
-flake8 $1
+flake8 $1 --ignore E402
 
 echo "${COLOUR}"
-echo "IMPORT ORDER CHECK"
+echo "IMPORT ORDER CHECK (isort)"
 echo $NC
-isort $1 --check-only --diff
+isort $1 --check-only --diff --force-single-line-imports
 
 echo "${COLOUR}"
-echo "FORMAT CHECK"
+echo "FORMAT CHECK (black)"
 echo $NC
-black $1 --check --diff
+black $1 --check --diff --line-length=79 --color
 
 echo "${COLOUR}"
-echo "TYPE CHECK"
+echo "TYPE CHECK (mypy)"
 echo $NC
 mypy $1
